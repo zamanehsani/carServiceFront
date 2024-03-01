b9545b66b9a380a80c43f7ae016a2abd85d1f3c7
Zaman Ehsani <zamanehsani@gmail.com>
2024-03-01 16:26:23 +0400

    added the cleared the forms for both expense and deals

--- src/components/expense/index.jsx
+++ src/components/expense/index.jsx
@@ -1,23 +1,22 @@
-import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import axios from 'axios'; // Import axios
 import Form from './form';
 import FormSubmissionSuccess from './formSuccess';
 
-
 export default function Expense(){
-    const auth = useSelector((state)=>state.auth)
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
 
-    useEffect(()=>{ return ()=>{setError(null);}
+    useEffect(()=>{ 
+        return ()=>{
+            setError(false);
+            setSuccess(false);
+        }
     },[])
 
     return ( <>
         {success ?  <FormSubmissionSuccess message={success} setSuccess={setSuccess}/> :
             <div className="flex flex-col items-center justify-center py-7 px-4">
-            <Form success={success} setSuccess={setSuccess} error={error} setError={setError} />
+                <Form success={success} setSuccess={setSuccess} error={error} setError={setError} />
             </div>}
     </>
            
