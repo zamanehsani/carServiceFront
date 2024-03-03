c25b96ad79b27b43653c25ba714a5080dc98af1a
Zaman Ehsani <zamanehsani@gmail.com>
2024-03-04 02:14:27 +0400

    some minor refactoring

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -19,6 +19,7 @@ export default function Home(){
             <SalesList />
 
             {/* the list of expense */}
+            {/* check if the auth.user.is active */}
             <ExpensesList />
 
         </div>
