4b1c32af70adde376223ceebba37ed1c8fe6ef5f
Zaman Ehsani <zamanehsani@gmail.com>
2024-03-06 19:02:43 +0400

    added the remove model for expenses.

--- src/components/home/dashboardComponents/expenses/expenseDetails.jsx
+++ src/components/home/dashboardComponents/expenses/expenseDetails.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom"
 import { useSelector } from "react-redux";
 import axios from 'axios'; 
 
+import RemoveItemModal from "./expenseRemove";
 
 export default function ExpenseDetails({match}){
     const {id} = useParams(); 
@@ -59,8 +60,6 @@ export default function ExpenseDetails({match}){
             <div className="m-2 bg-slate-100 rounded-md shadow-md py-5 px-5">
                  
                 <h1 className="font-bold leading-10 text-2xl text-indigo-600">Expense Details:</h1>
-                
-
 
                 <p className="text-gray-600"><span className="font-semibold mx-2 text-indigo-600 ">Invoice Number: </span>{exp?.invoice_number}</p>
                 <p className="text-gray-600"><span className="font-semibold mx-2">Date of Purchase: </span>{exp?.date}</p>
@@ -69,7 +68,7 @@ export default function ExpenseDetails({match}){
                 <p className="text-gray-600"><span className="font-semibold mx-2">Price: </span > AED {exp?.price}</p>
                 
                 {/* Edit and Delete icons */}
-                <div className="">
+                <div className="flex">
                         {hasPermission('change_invoice') &&
                         <button className="m-3" onClick={()=>nagivate(`/expense-edit/${exp.id}`)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-indigo-500 w-8 h-8">
@@ -78,14 +77,8 @@ export default function ExpenseDetails({match}){
                             </svg>
                         </button> }
                         
-                        {hasPermission('delete_invoice') &&
-                        <button className="m-3" onClick={()=>console.log("remove this")}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-red-500 w-8 h-8">
-                                <path strokeLinecap="round" strokeLinejoin="round" 
-                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>
-                        </button> }
-                    </div>
+                        {hasPermission('delete_invoice') && <RemoveItemModal instance={exp} />}
+                </div>
 
                 <hr className="my-5 border-gray-100"/>
                 <h1 className="font-bold leading-10 text-2xl text-indigo-600">Note</h1>
